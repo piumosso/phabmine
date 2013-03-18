1d25d5e8af6b52d3bdbc990e2f7905736400128d
Redmine <redmine@fcolors.ru>
2013-03-18 14:47:00 +0400

    Integrated with ruby-phabricator and phabricator

--- lib/phabricator_audits_hook_listener.rb
+++ lib/phabricator_audits_hook_listener.rb
@@ -1,22 +1,30 @@
+# encoding: utf-8
 require 'json'
+require_relative '../ruby-phabricator/shortcuts.rb'
+
 
 class PollsHookListener < Redmine::Hook::ViewListener
   def view_issues_show_details_bottom(context={} )
     def get_changesets_statuses(changesets)
       # Ask Phabricator for statuses
-      return Hash[
-        "93c8bc77d269d52fa8e25bde891532d37315791a" => "accepted",
-        "dc169d85d6e93d05386a7ff9a82cf3996d2bf59a" => "raised_concern"
-      ].to_json
+	print 'chs', changesets, changesets.empty?, changesets.nil?
+      if changesets.empty?
+	return {}
+      else
+        data = get_commit_status 'PAT', changesets, '/home/redmine/redmine_app/plugins/redmine_phabricator_audits/.arcrc'
+      end
+#      data = changesets
+      return data.to_json
     end
     issue = Issue.find(context[:issue])
-    statuses = get_changesets_statuses(issue.changesets)
-    return "<style type=\"text/css\">.accepted{background-color:green;}.raised_concern{background-color:red;}.audit_info{display:inline-block;width:10px;height:10px;border-radius:5px;margin-left:5px;}</style>" + 
-           "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js\"></script>" + 
-           "<script>$(function(){ var el = $(\'#phabricator_audit_info\').data('statuses'); console.log(el['93c8bc77d269d52fa8e25bde891532d37315791a']);" +
-           "$.each(el, function(key, value){console.log('!');console.log(\'a[title=\ \"Ревизия \' + key.substring(0,8) + \'\"]\');" +
-           "$(\'a[title=\"Ревизия \' + key.substring(0,8) + \'\"]\').before('<span class=\"audit_info ' + value + '\"></span>')})});" + 
+    print issue
+    statuses = get_changesets_statuses(issue.changesets.map{|c| c.revision})
+    return "<style type=\"text/css\">.in_progress{background-color:gray;}.accepted{background-color:green;}.conserned{background-color:red;}.audit_info{display:inline-block;width:10px;height:10px;border-radius:5px;margin-left:2px;margin-right:5px;}</style>" + 
+#           "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js\"></script>" + 
+           "<script>$(function(){ var el = $('#phabricator_audit_info').data('statuses');" +
+           "$.each(el, function(key, value){" +
+           "$('a[title=\"Ревизия ' + key.substring(4,12) + '\"]').before('<span class=\"audit_info ' + value + '\"></span>')})});" + 
 	   "</script>" + 
-           "<div id=\"phabricator_audit_info\"style=\"display:none\"data-statuses=\'#{statuses}\'></div>"
+           "<div id=\"phabricator_audit_info\"style=\"display:none\"data-statuses='#{statuses}'></div>"
   end
 end
