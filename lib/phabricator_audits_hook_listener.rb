f314243773bf5f44a506dd48b0319808d3223011
Redmine <redmine@fcolors.ru>
2013-03-19 18:09:57 +0400

    Revision urls leads to phabricator.

--- lib/phabricator_audits_hook_listener.rb
+++ lib/phabricator_audits_hook_listener.rb
@@ -5,25 +5,39 @@ require_relative '../ruby-phabricator/shortcuts.rb'
 
 class PollsHookListener < Redmine::Hook::ViewListener
   def view_issues_show_details_bottom(context={} )
-    def get_changesets_statuses(changesets)
+    def get_changesets_statuses(changesets, project_sid)
       # Ask Phabricator for statuses
-	print 'chs', changesets, changesets.empty?, changesets.nil?
       if changesets.empty?
 	return {}
       else
-        data = get_commit_status 'PAT', changesets, '/home/redmine/redmine_app/plugins/redmine_phabricator_audits/.arcrc'
+        data = get_commit_status project_sid, changesets, '/home/redmine/redmine_app/plugins/redmine_phabricator_audits/.arcrc'
       end
-#      data = changesets
       return data.to_json
     end
+
+    def get_phabricator_project_slug(redmine_slug)
+      redmine_phabricator_project_mapping = {
+          "0560ru" => "PAT",
+          "fut2" => "FUT",
+      }
+      if redmine_phabricator_project_mapping.include? redmine_slug
+        return redmine_phabricator_project_mapping[redmine_slug]
+      else
+        return redmine_slug.upcase
+      end
+    end
+
     issue = Issue.find(context[:issue])
-    print issue
-    statuses = get_changesets_statuses(issue.changesets.map{|c| c.revision})
+    project_sid = get_phabricator_project_slug(issue.project.identifier)
+    statuses = get_changesets_statuses(issue.changesets.map{|c| c.revision}, project_sid)
     return "<style type=\"text/css\">.in_progress{background-color:gray;}.accepted{background-color:green;}.conserned{background-color:red;}.audit_info{display:inline-block;width:10px;height:10px;border-radius:5px;margin-left:2px;margin-right:5px;}</style>" + 
-#           "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js\"></script>" + 
            "<script>$(function(){ var el = $('#phabricator_audit_info').data('statuses');" +
+           "var reg = /r([A-Z0-9]+)([a-z0-9]{40})/;" +
            "$.each(el, function(key, value){" +
-           "$('a[href*=\"/revisions/' + key.substring(4,12) + '\"]').before('<span class=\"audit_info ' + value + '\"></span>')})});" + 
+           "var $rev_url = $('a[href*=\"/revisions/' + key.replace(reg,'$2') + '\"]');" + 
+           "$rev_url.before('<span class=\"audit_info ' + value['status'] + '\"></span>');" +
+           "if(value['url']){$rev_url.attr('href', value['url']).attr('target', 'blank');};" +
+           "})});" + 
 	   "</script>" + 
            "<div id=\"phabricator_audit_info\"style=\"display:none\"data-statuses='#{statuses}'></div>"
   end
--- test.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-
-require_relative 'ruby-phabricator/shortcuts.rb'
-
-print get_commit_status 'РАТ', ['209a3ffa991169db6f273a3eedfb5f7ad735430b', 'd26e6e20126006d60ce3cb2024f53330b8bc8329']
