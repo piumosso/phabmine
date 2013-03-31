de21b90bfac6cf0cf71593d5b2feca05131b1f88
Melevir <melevir@gmail.com>
2013-03-31 22:16:12 +0400

    .arcrc file moved to env variable with default place.

--- lib/phabricator_audits_hook_listener.rb
+++ lib/phabricator_audits_hook_listener.rb
@@ -8,9 +8,10 @@ class PollsHookListener < Redmine::Hook::ViewListener
     def get_changesets_statuses(changesets, project_sid)
       # Ask Phabricator for statuses
       if changesets.empty?
-	return {}
+  return {}
       else
-        data = get_commit_status project_sid, changesets, '/home/redmine/redmine_app/plugins/redmine_phabricator_audits/.arcrc'
+        arcrc_path = ENV['PHABMINE_ARCRC_PATH'] || File.expand_path('./plugins/phabmine/.arcrc')
+        data = get_commit_status project_sid, changesets, arcrc_path
       end
       return data.to_json
     end
@@ -30,15 +31,15 @@ class PollsHookListener < Redmine::Hook::ViewListener
     issue = Issue.find(context[:issue])
     project_sid = get_phabricator_project_slug(issue.project.identifier)
     statuses = get_changesets_statuses(issue.changesets.map{|c| c.revision}, project_sid)
-    return "<style type=\"text/css\">.in_progress{background-color:gray;}.accepted{background-color:green;}.conserned{background-color:red;}.audit_info{display:inline-block;width:10px;height:10px;border-radius:5px;margin-left:2px;margin-right:5px;}</style>" + 
+    return "<style type=\"text/css\">.in_progress{background-color:gray;}.accepted{background-color:green;}.conserned{background-color:red;}.audit_info{display:inline-block;width:10px;height:10px;border-radius:5px;margin-left:2px;margin-right:5px;}</style>" +
            "<script>$(function(){ var el = $('#phabricator_audit_info').data('statuses');" +
            "var reg = /r([A-Z0-9]+)([a-z0-9]{40})/;" +
            "$.each(el, function(key, value){" +
-           "var $rev_url = $('a[href*=\"/revisions/' + key.replace(reg,'$2') + '\"]');" + 
+           "var $rev_url = $('a[href*=\"/revisions/' + key.replace(reg,'$2') + '\"]');" +
            "$rev_url.before('<span class=\"audit_info ' + value['status'] + '\"></span>');" +
            "if(value['url']){$rev_url.attr('href', value['url']).attr('target', 'blank');};" +
-           "})});" + 
-	   "</script>" + 
+           "})});" +
+     "</script>" +
            "<div id=\"phabricator_audit_info\"style=\"display:none\"data-statuses='#{statuses}'></div>"
   end
 end
