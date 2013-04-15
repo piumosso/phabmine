f0a7da6ef037ed375ef333cbcc8dab849173289a
Melevir <melevir@gmail.com>
2013-04-15 22:34:59 +0400

    CI settings changed.

--- test/phabmine_helpers_test.rb
+++ test/phabmine_helpers_test.rb
@@ -7,4 +7,8 @@ class PollsControllerTest < ActionController::TestCase
     assert_equal settings['roles_to_hide_phabricator_url'], []
     assert_equal settings['redmine_phabricator_project_mapping'], {}
   end
+
+  def test_that_fails
+    assert_equal 1, 2
+  end
 end
