cf18806420e2e32a61015cae0eea1fbdbf9f8206
Melevir <melevir@gmail.com>
2013-04-17 01:22:37 +0400

    CI settings changed

--- test/phabmine_helpers_test.rb
+++ test/phabmine_helpers_test.rb
@@ -1,4 +1,4 @@
-require_relative 'test_helper'
+require_relative '../test/test_helper.rb'
 require_relative '../lib/phabricator_audits_hook_listener.rb'
 
 class PollsControllerTest < ActionController::TestCase
