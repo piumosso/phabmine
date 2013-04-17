8ad6b1bf887ad40613515a09c5966a805793d77b
Melevir <melevir@gmail.com>
2013-04-17 09:07:56 +0400

    CI settings changed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 # Load the normal Rails helper
-require_relative '../../../test/test_helper.rb')
+require_relative '../../../test/test_helper.rb'
 
 # Ensure that we are using the temporary fixture path
 # Engines::Testing.set_fixture_path
