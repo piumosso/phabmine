951eabfc4965d6137760b17a6bb6398bdcfa5825
Melevir <melevir@gmail.com>
2013-04-26 16:10:28 +0400

    Commit branches shows up v3 (showing branches affected by ticket).

--- assets/javascripts/statuses.js
+++ assets/javascripts/statuses.js
@@ -2,26 +2,37 @@ $(function(){
     var el = $('#phabricator_audit_info').data('statuses'),
         reg = /r([A-Z0-9]+)([a-z0-9]{40})/,
         change_url = $('#phabricator_audit_info').data('change-url'),
-        project_sid = $('#phabricator_audit_info').data('project-sid');
+        project_sid = $('#phabricator_audit_info').data('project-sid'),
+        el_length = Object.keys(el).length,
+        commits_counter = 1;
     $.each(el, function(key, value){
         var commit_id = key.replace(reg,'$2'),
-            $rev_url = $('a[href*=\"/revisions/' + commit_id + '\"]');
+            $rev_url = $('a[href*=\"/revisions/' + commit_id + '\"]'),
+            show_final_branches = commits_counter == el_length;
         $rev_url.before('<span class=\"audit_info ' + value['status'] + '\"></span>');
         get_and_fill_commit_branches(project_sid, commit_id, $rev_url);
-
+        if (show_final_branches){
+            get_and_show_final_branch();
+        }
         if(change_url && value['url']){
             $rev_url.attr('href', value['url']).attr('target', 'blank');
         };
+        commits_counter += 1;
     })
 });
 
 
 function get_and_fill_commit_branches(project_sid, commit_id, $rev_url){
+    $branches_storage = $('#phabricator_audit_info')
     $.ajax({
         url: '/phabmine/'+ project_sid + '/commit/' + commit_id + '/branches/',
-        success: function(data){
-            fill_branches($rev_url, $.parseJSON(data));
-        }
+        async: false,
+    }).then(function(data){
+        data = $.parseJSON(data);
+        fill_branches($rev_url, data);
+        old_data = $branches_storage.data('branches') || [];
+        new_branches = old_data.concat(data);
+        $branches_storage.data('branches', new_branches)
     })
 }
 
@@ -35,4 +46,16 @@ function fill_branches($rev_url, branches)
           $branches_info.append('<span class="branch">' + value + '</span>')
       })
     }
+}
+
+function get_and_show_final_branch()
+{
+    $branches_storage = $('#phabricator_audit_info');    
+    branches = $branches_storage.data('branches');
+    unique = $.unique(branches);
+    $('#issue-changesets').find('.changeset').first().before('<div id="issue_branches"></div>');
+    var $issue_branches = $('#issue_branches');
+    $.each(unique, function(i, branch){
+        $issue_branches.append('<span class="branch">' + branch + '</span>')
+    })
 }
