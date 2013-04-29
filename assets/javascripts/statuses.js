82ae92628fbae0be8453dd24657e6a18112e3578
Melevir <melevir@gmail.com>
2013-04-29 23:35:41 +0400

    js refactoring

--- assets/javascripts/statuses.js
+++ assets/javascripts/statuses.js
@@ -1,108 +1,151 @@
 $(function(){
-    var $phabricator_audit_info = $('#phabricator_audit_info'),
-        el = $('#phabricator_audit_info').data('statuses'),
-        reg = /r([A-Z0-9]+)([a-z0-9]{40})/,
-        is_gitflow_project = $phabricator_audit_info.data('is-gitflow-project'),
-        change_url = $phabricator_audit_info.data('change-url'),
-        project_sid = $phabricator_audit_info.data('project-sid'),
-        show_commit_branches = $phabricator_audit_info.data('show-commits-branches'),
-        show_tickets_branches = $phabricator_audit_info.data('show-tickets-branches'),
-        instance_branch_mapping = $phabricator_audit_info.data('instance-branch-mapping'),
-        el_length = Object.keys(el).length,
-        commits_counter = 1;
-    $.each(el, function(key, value){
-        var commit_id = key.replace(reg,'$2'),
-            $rev_url = $('a[href*=\"/revisions/' + commit_id + '\"]'),
-            show_final_branches = commits_counter == el_length;
-        $rev_url.before('<span class=\"audit_info ' + value['status'] + '\"></span>');
-        if (show_commit_branches || show_tickets_branches){
-            get_and_fill_commit_branches(project_sid, commit_id, $rev_url, show_commit_branches);
+    var getAndFillCommitBranches = function(projectSid, commitId, $revUrl, toFill){
+        var $info = $('#phabricator_audit_info'),
+            oldData, newBranches;
+
+        $.ajax({
+            url: '/phabmine/'+ projectSid + '/commit/' + commitId + '/branches/',
+            async: false
+        }).then(function(data){
+                data = $.parseJSON(data);
+                if(toFill){
+                    fillBranches($revUrl, data);
+                }
+                oldData = $info.data('branches') || [];
+                newBranches = oldData.concat(data);
+                $info.data('branches', newBranches)
+            })
+    };
+
+    var fillBranches = function($revUrl, branches){
+        var $branchesInfo;
+
+        $revUrl
+            .parent()
+            .siblings('.wiki')
+            .after('<div class="branches_info"></div>');
+
+        $branchesInfo = $revUrl.parents('.changeset').find('.branches_info');
+
+        if(branches){
+            $.each(branches, function(key, value){
+                $branchesInfo.append('<span class="branch">' + value + '</span>')
+            })
         }
-        if (show_final_branches && show_tickets_branches){
-            get_and_show_final_branch(instance_branch_mapping, is_gitflow_project);
+    };
+
+   var getAndShowFinalBranch = function(instanceBranchMapping, isGitflowProject){
+        var headerText = isGitflowProject? 'Instance' : 'Affected branches',
+            $info = $('#phabricator_audit_info'),
+            unique = $.unique($info.data('branches')),
+            projectSid = $info.data('projectSid'),
+            $issueBranches,
+            elderBranch,
+            instanceName;
+
+       $('#issue-changesets')
+            .prepend('<h3 id="branches_header">' + headerText + '</h3>');
+
+       $('#branches_header')
+            .next()
+            .before('<div id="issue_branches"></div>');
+
+        $issueBranches = $('#issue_branches');
+        if (isGitflowProject){
+            elderBranch = getGitflowElderBranch(unique);
+            if(projectSid in instanceBranchMapping){
+                instanceName = instanceBranchMapping[projectSid][elderBranch[0]] || '';
+            }
+            else{
+                instanceName = '';
+            }
+            $issueBranches
+                .append(
+                    '<div>' +
+                        '<a target="_blank" class="elder_branch" href="http://' + instanceName + '">' + instanceName + '</a>' +
+                        '<span class="elder_branch_name">' + elderBranch[1] + '</span>' +
+                    '</div>'
+            );
         }
-        if(change_url && value['url']){
-            $rev_url.attr('href', value['url']).attr('target', 'blank');
-        };
-        commits_counter += 1;
-    })
-});
-
-
-function get_and_fill_commit_branches(project_sid, commit_id, $rev_url, to_fill){
-    $branches_storage = $('#phabricator_audit_info')
-    $.ajax({
-        url: '/phabmine/'+ project_sid + '/commit/' + commit_id + '/branches/',
-        async: false,
-    }).then(function(data){
-        data = $.parseJSON(data);
-        if(to_fill){
-            fill_branches($rev_url, data);
+        else{
+            $.each(unique, function(i, branch){
+                $issueBranches.append('<span class="branch">' + branch + '</span>')
+            })
         }
-        old_data = $branches_storage.data('branches') || [];
-        new_branches = old_data.concat(data);
-        $branches_storage.data('branches', new_branches)
-    })
-}
+    };
 
+    var getGitflowElderBranch = function(branches){
+        var elderBranch,
+            elderBranchType,
+            curentElderBranch;
 
-function fill_branches($rev_url, branches)
-{
-    $rev_url.parent().siblings('.wiki').after('<div class="branches_info"></div>');    
-    var $branches_info = $rev_url.parents('.changeset').find('.branches_info');
-    if(branches){
-      $.each(branches, function(key, value){
-          $branches_info.append('<span class="branch">' + value + '</span>')
-      })
-    }
-}
+        $(['master', 'hotfix', 'release', 'dev', 'feature'])
+            .each(function(i, branchId){
+                curentElderBranch = branchesContain(branches, branchId);
+                if (curentElderBranch){
+                    elderBranch = curentElderBranch;
+                    elderBranchType = branchId;
+                    return false;
+                }
+            });
 
-function get_and_show_final_branch(instance_branch_mapping, is_gitflow_project)
-{
-    var header_text = is_gitflow_project? 'Instance' : 'Affected branches',
-        $branches_storage = $('#phabricator_audit_info')
-        branches = $branches_storage.data('branches')
-        unique = $.unique(branches),
-        $changesets = $('#issue-changesets'),
-        project_sid = $branches_storage.data('project-sid'),
-    
-    $changesets.prepend('<h3 id="branches_header">' + header_text + '</h3>');
-    var $branches_header = $('#branches_header');
-    $branches_header.next().before('<div id="issue_branches"></div>');
-    var $issue_branches = $('#issue_branches');
-    if (is_gitflow_project){
-        var elder_branch = get_gitflow_elder_branch(unique),
-            instance_name = project_sid in instance_branch_mapping? instance_branch_mapping[project_sid][elder_branch[0]] || '': '';
-        $issue_branches.append('<div><a target="blank" class="elder_branch" href="http://' + instance_name + '">' + instance_name + '</a><span class="elder_branch_name">' + elder_branch[1] + '</span></div>');
-    }
-    else{
-        $.each(unique, function(i, branch){
-            $issue_branches.append('<span class="branch">' + branch + '</span>')
-        })
-    }
-}
+        return [elderBranchType, elderBranch]
+    };
 
-function get_gitflow_elder_branch(branches){
-    var elder_branch = '',
-        elder_branch_type = '';
-    $(['master', 'hotfix', 'release', 'dev', 'feature']).each(function(i, branch_id){
-        curent_elder_branch = branches_contain(branches, branch_id)
-        if (curent_elder_branch){
-            elder_branch = curent_elder_branch;
-            elder_branch_type = branch_id;
+    var branchesContain = function(branches, branchId){
+        var contain,
+            branch;
+
+        $(branches).each(
+            function(i, b){
+                if(b.substring(0, branchId.length) == branchId){
+                    contain = true;
+                    branch = b;
+                }
+            }
+        );
+
+        if(contain){
+            return branch
+        }else{
             return false;
         }
-    })
-        return [elder_branch_type, elder_branch]
-}
+    };
 
-function branches_contain(branches, branch_id){
-    var contain = false,
-        branch = '';
-    $(branches).each(function(i, b){ if(b.substring(0, branch_id.length) == branch_id){ contain = true; branch = b}})
-    if(contain){
-        return branch
-    }else{
-        return contain;
-    }
-}
+
+    var $info = $('#phabricator_audit_info'),
+        el = $info.data('statuses'),
+        showCommitBranches = $info.data('showCommitsBranches'),
+        showTicketsBranches = $info.data('showTicketsBranches'),
+        elLength = Object.keys(el).length,
+        commitsCounter = 1,
+        commitId, $revUrl, showFinalBranches;
+
+    $.each(el, function(key, value){
+        commitId = key.replace(/r([A-Z0-9]+)([a-z0-9]{40})/,'$2');
+        $revUrl = $('a[href*=\"/revisions/' + commitId + '\"]');
+        showFinalBranches = commitsCounter == elLength;
+
+        $revUrl.before('<span class=\"audit_info ' + value['status'] + '\"></span>');
+
+        if (showCommitBranches || showTicketsBranches){
+            getAndFillCommitBranches(
+                $info.data('project-sid'),
+                commitId,
+                $revUrl,
+                showCommitBranches
+            );
+        }
+        if (showFinalBranches && showTicketsBranches){
+            getAndShowFinalBranch($info.data('instanceBranchMapping'), $info.data('isGitflowProject'));
+        }
+
+        if($info.data('change-url') && value['url']){
+            $revUrl
+                .attr('href', value['url'])
+                .attr('target', '_blank');
+        }
+
+        commitsCounter += 1;
+    });
+});
